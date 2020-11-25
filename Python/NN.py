0bba906c84419abf10b1c3ddb48f09476d78a9e6
jaguargirl <61291322+jaguargirl@users.noreply.github.com>
2020-11-25 20:21:15 +0200

    Update NN.py

--- Python/NN.py
+++ Python/NN.py
@@ -8,13 +8,13 @@ def activare(x, deriv=False):
 x = np.array([[1, 0, 0, 1],[1, 0, 1, 1], [1, 1, 0, 10], [1, 1, 1, 1]])
 y = np.array([[0, 0, 1, 1]]).T
 np.random.seed(1)
-w0 = np.random.random((4, 1))*2-1 #3 ponderi aleatoare intre -1 si 1 => (-1,1)
-for i in range(10000): #nr epoci
+w0 = np.random.random((4, 1))*2-1
+for i in range(10000):
     l0 = x
     l1 = activare(np.dot(l0, w0)) #out y
     l1_err = y-l1
     l1_delta = 0.1*l1_err*activare(l1, True) #dEr_tot/dg(y)*g(y)*(1-g(y))
     w0 += np.dot(l0.T, l1_delta)
 
-print("Dupa antrenare obinem: ")
+print("After training we get: ")
 print(l1)

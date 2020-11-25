b633eae3745384594670ebd490a0dc94b526d347
jaguargirl <61291322+jaguargirl@users.noreply.github.com>
2020-11-25 20:30:35 +0200

    Update Eigenfaces.m

--- Functions/Eigenfaces.m
+++ Functions/Eigenfaces.m
@@ -1,14 +1,14 @@
-function pozitia=Eigenfaces(A,training,poza,norma,media,hqb,proiectii)
+function position=Eigenfaces(A,training,img,norm,med,hqb,proiections)
 Z=zeros(1,training*40);
-poza=double(reshape(poza,10304,1));
-poza=poza-media;
-pr_poza=poza'*hqb;
+img=double(reshape(img,10304,1));
+img=img-med;
+img_pr=img'*hqb;
 for i=1:size(A,2)
-    switch norma
-        case 'n1',Z(i)=norm(pr_poza-proiectii(i,:),1);
-        case 'n2',Z(i)=norm(pr_poza-proiectii(i,:),2);
-        case 'ninf',Z(i)=norm(pr_poza-proiectii(i,:),inf);
-        case 'ncos',Z(i)=1-dot(pr_poza,proiectii(i,:))/(norm(pr_poza)*norm(proiectii(i,:)));
+    switch norm
+        case 'n1',Z(i)=norm(img_pr-proiectiions(i,:),1);
+        case 'n2',Z(i)=norm(img_pr-proiections(i,:),2);
+        case 'ninf',Z(i)=norm(img_pr-proiections(i,:),inf);
+        case 'ncos',Z(i)=1-dot(img_pr,proiections(i,:))/(norm(img_pr)*norm(proiections(i,:)));
     end
 end
-[distanta,pozitia]=min(Z);
+[distance,position]=min(Z);
